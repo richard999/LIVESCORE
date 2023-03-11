420ec44a4ab3cd677fa33f23e9f2d544536b052b
richard999 <ebanksrichard22@gmail.com>
2023-03-11 11:06:14 -0500

    Add project files.

---
+++ Models/Class.cs
@@ -0,0 +1,14 @@
+﻿namespace LIVESCORE.Models
+{
+    public class FootballMatch
+    {
+        private string hometeam;
+        private string awayteam;
+        private int matchtime;
+        private string starttime;
+        private bool isfinished;
+        private string hometeamscore;
+        private string awayteamscore;
+    }
+
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LIVESCORE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
